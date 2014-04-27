55da22ede2e2234ff2b928e6cea0159b29d9528d
Piotr Szul <piotr.szul@csiro.au>
2014-04-27 21:37:59 +1000

    working version

--- run_analysis.R
+++ run_analysis.R
@@ -5,8 +5,16 @@ isStdFeature <- sapply(features, function(s) { length(grep("std\\(\\)",s)) >0})
 isMeanOrStdFeature <- isMeanFeature | isStdFeature
 srcActivityLabels <- read.table("activity_labels.txt")
 activityLables <- srcActivityLabels[[2]]
-#srcTestData <- read.table("test/X_test.txt")
-#srcTestActivities <-read.table("test/y_test.txt")
+srcTestData <- rbind(read.table("test/X_test.txt"), read.table("train/X_train.txt"))
+srcTestActivities <-rbind(read.table("test/y_test.txt"), read.table("train/y_train.txt"))
 activities <- lapply(srcTestActivities, function (i) {activityLables[i]})
 tidyData <- cbind(activities, srcTestData[isMeanOrStdFeature])
 colnames(tidyData) <- c("Activity", as.character(features[isMeanOrStdFeature]))
+write.table(tidyData,"tidySet1.txt")
+##
+srcSubjects <- rbind(read.table("test/subject_test.txt", col.names="Subject"), read.table("train/subject_train.txt", col.names="Subject"))
+tidyDataWithSubject <- cbind(srcSubjects, tidyData)
+tidyData2 <- aggregate(. ~ Activity + Subject, data= tidyDataWithSubject, mean)
+library(plyr)
+tidyData2 <- arrange(tidyData2, Activity, Subject)
+write.table(tidyData2,"tidySet2.txt")
